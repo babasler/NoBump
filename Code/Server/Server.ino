0280dc22f9e77e6a5d53b87bf91279a2a08b932b
Bastian Basler <bastian.basler2002@gmail.com>
2025-09-03 19:20:27 +0200

    Server done

--- Code/Server/Server.ino
+++ Code/Server/Server.ino
@@ -6,19 +6,39 @@
 #include <BLEDevice.h>
 #include <BLEUtils.h>
 #include <BLEServer.h>
+#include <string.h>
+
+using namespace std;
 
 #define SERVICE_UUID        "4fafc201-1fb5-459e-8fcc-c5c9c331914b"
 #define CHARACTERISTIC_UUID "beb5483e-36e1-4688-b7f5-ea07361b26a8"
 
+#define DOOR_OPEN "OPEN"
+#define DOOR_CLOSED "CLOSED"
+
+#define LED_PIN 2
+#define TICKS 5
+#define DELAY_MS 50
+
+#define OK 0
+#define UNKNOWN_COMMAND -1
+
 BLECharacteristic *pCharacteristic;
 
+void testLed(uint8_t pin, uint8_t ticks);
+int8_t turnOnWarnSignalIfNessecary(String signal);
 
-class MyCallbacks : public BLECharacteristicCallbacks {
+
+class serverCallbacks : public BLECharacteristicCallbacks {
     void onWrite(BLECharacteristic *pCharacteristic) {
         String value = pCharacteristic->getValue();
         if (value.length() > 0) {
             Serial.print("Befehl empfangen: ");
             Serial.println(value.c_str());
+            int8_t ret = turnOnWarnSignalIfNessecary(value);
+            if(ret < 0){
+              Serial.println("Recieved unkown command");
+            }
         }
     }
 };
@@ -27,25 +47,56 @@ void setup() {
   Serial.begin(115200);
   Serial.println("Starting BLE work!");
 
-  BLEDevice::init("Long name works now");
+  pinMode(LED_PIN,OUTPUT);
+
+  BLEDevice::init("NoBump Server");
   BLEServer *pServer = BLEDevice::createServer();
   BLEService *pService = pServer->createService(SERVICE_UUID);
   pCharacteristic = pService->createCharacteristic(CHARACTERISTIC_UUID, BLECharacteristic::PROPERTY_READ | BLECharacteristic::PROPERTY_WRITE);
 
-  //pCharacteristic->setValue("Hello World says Neil");
-  pCharacteristic->setCallbacks(new MyCallbacks());
+  pCharacteristic->setCallbacks(new serverCallbacks());
   pService->start();
-  // BLEAdvertising *pAdvertising = pServer->getAdvertising();  // this still is working for backward compatibility
+
   BLEAdvertising *pAdvertising = BLEDevice::getAdvertising();
   pAdvertising->addServiceUUID(SERVICE_UUID);
   pAdvertising->setScanResponse(true);
-  pAdvertising->setMinPreferred(0x06);  // functions that help with iPhone connections issue
   pAdvertising->setMinPreferred(0x12);
   BLEDevice::startAdvertising();
-  Serial.println("Characteristic defined! Now you can read it in your phone!");
+  Serial.println("Characteristic defined");
+
+  //Test LED
+  Serial.println("Testing LED now");
+  testLed(LED_PIN,TICKS);
+
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
   delay(2000);
 }
+
+void testLed(uint8_t pin, uint8_t ticks){
+  for(uint8_t i = 0; i<ticks; i++){
+    digitalWrite(pin,HIGH);
+    delay(1000);
+    digitalWrite(pin,LOW);
+    delay(1000);
+  }
+}
+
+int8_t turnOnWarnSignalIfNessecary(String signal){
+  if(signal == DOOR_OPEN){
+    digitalWrite(LED_PIN,HIGH);
+    delay(DELAY_MS);
+    return OK;
+  }
+  else if(signal == DOOR_CLOSED){
+    digitalWrite(LED_PIN,LOW);
+    delay(DELAY_MS);
+    return OK;
+  }
+  else{
+    return UNKNOWN_COMMAND;
+  }
+
+}
+
--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # NoBump
+
+Der Server ist derjenige der Daten (Befehle) empfängt.
+Der Client gibt die Daten (Befehle) an den Server weiter.
+
+Technisch ist es so, dass der BLE Server ein Service und in dem Service eine Charakteristic bereitstelt und der Client in diese Daten über Bluetooth schickt.
+
+Der Server soll entsprechend reagieren und je nach Befehl, ein Warn Signal an, oder ausschalten. 
+Der Client übernimmt die Datenverarbeitung und entscheidet, ob der Zustand so ist, das ein Befehl zum Anschalten der Warnleuchte erforderlich ist.
+Dazu misst der Client in einem Intervall von [X] Sekunden die Distanz und wenn sie größer als [X] cm ist, wird der entsprechende Befehl geschickt.
+
