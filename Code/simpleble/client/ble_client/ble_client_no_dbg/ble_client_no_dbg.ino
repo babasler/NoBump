d3c9a1011c9e4bd577064fa00df5ca2e68d81db0
Bastian Basler <bastian.basler2002@gmail.com>
2025-12-21 22:54:56 +0100

    mqtt in own thread

--- Code/simpleble/client/ble_client/ble_client_no_dbg/ble_client_no_dbg.ino
+++ Code/simpleble/client/ble_client/ble_client_no_dbg/ble_client_no_dbg.ino
@@ -3,20 +3,35 @@
 #include <PubSubClient.h>
 #include <WiFi.h>
 #include <secret.h>
-
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
 
 static const NimBLEAdvertisedDevice* advDevice;
+TaskHandle_t wifiTaskHandle = NULL;
 static bool doConnect = false;
 static uint32_t scanTimeMs = 5000;  // Scan duration in milliseconds 0 = forever
 
 static NimBLEUUID UUID_NOBUMP_SERVICE("AFFE");
 static NimBLEUUID UUID_DOORSTATE_CHAR("BEEF");
 
-const uint8_t ledPins[] = { 1, 2, 17, 19 };
+volatile bool doorStateProcessing = false;  // Flag für DoorState-Priorität
+
+const uint8_t ledPins[] = { 1, 2, 17, 20 };
+#define DEBUG
+
+#ifdef DEBUG
+#define DBG(x) Serial.println(x)
+#else
+#define DBG(x) do {} while (0)
+#endif
 
 enum DoorState : uint8_t {
   OPEN,
   CLOSED
+};
+
+struct TaskParams {
+  PubSubClient* mqttClient;
 };
 
 class ScanCallbacks : public NimBLEScanCallbacks {
@@ -35,11 +50,12 @@ void notifyCB(NimBLERemoteCharacteristic* pRemoteCharacteristic, uint8_t* pData,
     return;
   }
   if (length == 4 && memcmp(pData, "OPEN", 4) == 0) {
+    DBG("Revieved Open State");
     toggleLEDsFromDoorState(DoorState::OPEN);
   } else if (length == 6 && memcmp(pData, "CLOSED", 6) == 0) {
+    DBG("Revieved Closed State");
     toggleLEDsFromDoorState(DoorState::CLOSED);
   }
-  mqttPublishBattery();
 }
 bool connectToServer() {
   NimBLEClient* pClient = nullptr;
@@ -90,38 +106,54 @@ bool connectToServer() {
   return true;
 }
 
-void mqttPublishBattery() {
-  WiFi.begin(ssid, password);
+void sendBatteryTask(void *parameter) {
+  TaskParams* params = (TaskParams*)parameter;
+  PubSubClient* mqttClient = params->mqttClient;
+  uint8_t i = 1;
+  mqttClient->setServer("neptune4", 1883);
+  for (;;) {
+    // Warten auf BLE-Trigger
+    vTaskDelay(pdMS_TO_TICKS(5 * 60 * 1000)); // 5 Minuten warten
+    DBG("Waking up");
+     if (!doorStateProcessing) {  // nur wenn keine DoorState-Bearbeitung läuft
+      WiFi.mode(WIFI_STA);
+      WiFi.begin(ssid, password);
+      unsigned long start = millis();
+      while (WiFi.status() != WL_CONNECTED && millis() - start < 10000) {
+        vTaskDelay(pdMS_TO_TICKS(50));
+      }
 
-  unsigned long start = millis();
-  while (WiFi.status() != WL_CONNECTED && millis() - start < 5000) {
-    delay(100);
+      if (WiFi.status() == WL_CONNECTED) {
+        vTaskDelay(pdMS_TO_TICKS(500)); // 0,5 Sekunden Stabilisierung
+        if (mqttClient->connect("c6")) {
+          vTaskDelay(pdMS_TO_TICKS(100));
+          char buf[4];
+          itoa(i, buf, 10);  // Beispielwert
+          mqttClient->publish("noBump/battery", buf, true);
+          i++;
+          DBG("publish done");
+          vTaskDelay(pdMS_TO_TICKS(300)); // 100ms für TCP
+          mqttClient->disconnect();
+        }
+      }
+
+      WiFi.disconnect(false);
+      WiFi.mode(WIFI_OFF);
+      DBG("Bye Bye");
+    }
   }
-
-  static WiFiClient espClient;
-  static PubSubClient client(espClient);
-
-  if (WiFi.status() != WL_CONNECTED) return;  // Verbindung fehlgeschlagen
-
-  client.setServer("neptune4", 1883);
-
-  if (client.connect("c6")) {
-    char buf[4];
-    itoa(50, buf, 10);
-    client.publish("noBump/battery", buf, true);
-    client.disconnect();
-  }
-
-  WiFi.disconnect(true);
-  WiFi.mode(WIFI_OFF);
 }
 
 void toggleLEDsFromDoorState(DoorState state) {
+  doorStateProcessing = true;
   for (uint8_t i = 0; i < 4; i++) digitalWrite(ledPins[i], state == DoorState::OPEN ? HIGH : LOW);
+  doorStateProcessing = false;
 }
 
 void setup() {
   NimBLEDevice::init("NoBump-Client");
+
+  Serial.begin(115200);
 
   NimBLEDevice::setPower(3);
   NimBLEScan* pScan = NimBLEDevice::getScan();
@@ -131,11 +163,27 @@ void setup() {
   pScan->setActiveScan(false);
   pScan->start(scanTimeMs);
 
-  // Pins der LEDs als Output setzen
-  pinMode(1, OUTPUT);
-  pinMode(2, OUTPUT);
-  pinMode(17, OUTPUT);
-  pinMode(19, OUTPUT);
+  //WIFI Config
+  WiFi.mode(WIFI_STA);
+  WiFi.setSleep(true);           
+  WiFi.setAutoReconnect(true);
+
+  static WiFiClient wifiClient;
+  static PubSubClient mqttClient(wifiClient);
+
+  static TaskParams params = {&mqttClient };
+
+  //Thread für mqtt
+  xTaskCreatePinnedToCore(
+  sendBatteryTask,
+  "WiFiTask",
+  6144,
+  &params,
+  1,
+  &wifiTaskHandle,
+  0  
+  );
+  for (uint8_t i = 0; i < 4; i++) pinMode(ledPins[i], OUTPUT);
 
   // Pin für Spannungsmessung als Input setzen
   //pinMode(A0, INPUT);
